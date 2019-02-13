e610ddb58c88e60a3cf0574f28a82468be927b43
Stanislav Teroff <teroff13@gmail.com>
2019-02-12 17:23:17 -0800

    Two news challenges

---
+++ listRoation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+# check if list2 is a rotation of list1
+
+
+def isRotation(list1, list2):
+
+    if len(list1) != len(list2):
+        return False
+
+    key = list1[0]
+    key_counter = 0
+
+    for i in range(len(list2)):
+        if list2[i] == key:
+            key_counter = i
+            break
+
+    if key_counter == 0:
+        return False
+
+    for x in range(len(list1)):
+        l2_index = (key_counter+x) % len(list1)
+
+        if list1[x] != list2[l2_index]:
+            return  False
+
+    return True
+
+print(isRotation([1,2,3,4,5,6,7],[4,5,6,7,1,2,3]))
---
+++ reverse.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+
+# Reverse string
+#
+# start - This is super
+# finish - super is This
+
+def reverse(s):
+    if len(s) < len(s):
+        return "Lenght of the string is too short"
+
+    length = len(s)
+    words = []
+    spaces = [" "]
+    i = 0
+
+    while i < length:
+        if s[i] not in spaces:
+            wordstat = i
+
+        while i < length and s[i] not in spaces:
+            i += 1
+
+        words.insert(0, s[wordstat:i])
+        i += 1
+
+    return " ".join(words)
+
+
+t = "This is super"
+
+print(reverse(t))
